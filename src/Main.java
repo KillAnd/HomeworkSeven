30cef33549eac194fa83fa013019acd0508daf9b
kila1 <kila14081996@gmail.com>
2024-02-29 22:15:59 +0300

    Домашняя работа №7. Задание 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello.HomeworkSeven");
+//        task 1
+        int salary = 15000, total = 0, i = 0;
+        while (total <= 2_459_000) {
+            if (i==0) {
+                System.out.println("Месяц " + i + " сумма накоплений равна " + salary + " рублей.");
+            }
+            i++;
+            total = salary + total;
+            total = total + total/100;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей.");
+        }
+
+//        task 2
+
     }
 }
